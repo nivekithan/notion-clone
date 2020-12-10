25722f4b0ff7a931f88a73a03e51964a409eb8d4
Nivekithan S <nivekithan363@gmail.com>
2020-12-11 00:41:23 +0530

    finshed the new day component

--- memory/src/components/days.tsx
+++ memory/src/components/days.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { FaToggleOn, FaToggleOff } from "react-icons/fa";
+import { HiOutlinePlusCircle } from "react-icons/hi";
 
 // This element is child element for DayCards Element
 //
@@ -27,36 +28,37 @@ export const Days: React.FC = () => {
   }, []);
 
   const utility = {
-    "section" : [
-      "mx-10%"
-    ]
-  }
+    section: ["mx-10%"],
+  };
 
   return (
     <section className={utility.section.join(" ")}>
       <AddNewDays />
     </section>
-  )
+  );
 };
 // ---------------------------------------------------------------------------
 
-
 const AddNewDays = () => {
-
-  const utility = {
-
-    "bg-rectangle" : [
+  const utility  = {
+    "bg-rectangle": [
       "bg-myblue-400",
       "w-box340",
-      "h-box273"
-    ]
-
-  }
+      "h-box273",
+      "flex",
+      "items-center",
+      "justify-center",
+    ],
+    spanText: ["text-4xl", "text-white", "font-serif"],
+    headerText: ["flex", "flex-col", "items-center", "gap-y-4"],
+  };
 
   return (
     <section className={utility["bg-rectangle"].join(" ")}>
-
+      <div className={utility["headerText"].join(" ")}>
+        <div className={utility["spanText"].join(" ")}>New Day</div>
+        <HiOutlinePlusCircle color="#ffffff" size="27px" />
+      </div>
     </section>
-  )
-
-} 
+  );
+};
--- memory/tailwind.config.js
+++ memory/tailwind.config.js
@@ -7,37 +7,39 @@ module.exports = {
     "public/**/*.html",
   ],
   theme: {
+    fontFamily: {
+      "serif": ["Roboto", "sans-serif"],
+    },
     extend: {
       colors: {
-    
-        "mywhite": "#F8F9FA",
+        mywhite: "#F8F9FA",
         "mygrey-600": "#AEB9CC",
         "mygrey-400": "#B7C2D5",
-        "myblack-600" : "#565C63",
-        "myblack-400" : "#7B838E",
-        "myblue-400": "#304FD0"
+        "myblack-600": "#565C63",
+        "myblack-400": "#7B838E",
+        "myblue-400": "#304FD0",
       },
       margin: {
-        "10%" : "10%",
-        "20%" : "20%",
-        "30%" : "30%",
-        "40%" : "40%"
+        "10%": "10%",
+        "20%": "20%",
+        "30%": "30%",
+        "40%": "40%",
       },
-      spacing : {
-        sm : "8px",
-        l : "16px",
-        xl : "24px",
-        xxl : "36px",
-        xxxl : "48px",
-        "box340" : "340px",
-        "box273" : "273px"
+      spacing: {
+        sm: "8px",
+        l: "16px",
+        xl: "24px",
+        xxl: "36px",
+        xxxl: "48px",
+        box340: "340px",
+        box273: "273px",
       },
     },
   },
   variants: {
     extend: {
-      borderWidth: ["hover"]
-    }
+      borderWidth: ["hover"],
+    },
   },
   plugins: [],
 };
