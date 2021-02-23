e985d13d6846767b3044cb78efbebc6d3d097d2d
Nivekithan S <nivekithan363@gmail.com>
2021-02-23 18:12:11 +0530

    Updated the index choosing algrothim

--- notion-clone/src/editor/devtools/utils/withJSX.tsx
+++ notion-clone/src/editor/devtools/utils/withJSX.tsx
@@ -23,73 +23,80 @@ export function withJSX<T extends Editor>(editor: T): T {
       Transforms.setNodes(editor, { devtools_id: nanoid() }, { at: path });
     }
 
-    // Checking  first node in whole editor has devtools_index 1
-
     if (
-      !DevEditor.isEditor(node) &&
-      path.length === 1 &&
-      path[0] === 0 &&
-      !node.devtools_index
+      (!DevEditor.isEditor(node) && !node.devtools_index) ||
+      node.devtools_id !== path[path.length - 1]
     ) {
-      Transforms.setNodes(editor, { devtools_index: 1 }, { at: path });
+      Transforms.setNodes(editor, { devtools_index: path[path.length - 1] });
     }
 
-    // Checking if the node is first child of a node and making sure that its not first node in whole editor
-    if (
-      !DevEditor.isEditor(node) &&
-      path[path.length - 1] === 0 &&
-      path.length !== 1 &&
-      // Checking if the devtools_index is plus one of the devtools_index of its parent
+    // // Checking  first node in whole editor has devtools_index 1
 
-      Node.parent(editor, path).devtools_index !==
-        (node.devtools_index as number) - 1
-    ) {
-      console.group("First Child");
-      console.log({ node });
-      const { devtools_index } = Node.parent(editor, path);
-      console.log({ devtools_index });
-      Transforms.setNodes(
-        editor,
-        { devtools_index: (devtools_index as number) + 1 },
-        { at: path }
-      );
-      console.log(Node.get(editor, path));
-      console.groupEnd();
-    }
+    // if (
+    //   !DevEditor.isEditor(node) &&
+    //   path.length === 1 &&
+    //   path[0] === 0 &&
+    //   !node.devtools_index
+    // ) {
+    //   Transforms.setNodes(editor, { devtools_index: 1 }, { at: path });
+    // }
 
-    if (
-      !DevEditor.isEditor(node) &&
-      path[path.length - 1] !== 0 &&
-      findLastIndex(
-        Node.get(editor, [...path.slice(0, -1), path[path.length - 1] - 1])
-      ) +
-        1 !==
-        node.devtools_index
-    ) {
-      console.group("Other Child");
-      console.log({node})
-      console.log({
-        lastIndex: findLastIndex(
-          Node.get(editor, [...path.slice(0, -1), path[path.length - 1] - 1])
-        ),
-      });
+    // // Checking if the node is first child of a node and making sure that its not first node in whole editor
+    // if (
+    //   !DevEditor.isEditor(node) &&
+    //   path[path.length - 1] === 0 &&
+    //   path.length !== 1 &&
+    //   // Checking if the devtools_index is plus one of the devtools_index of its parent
 
-      Transforms.setNodes(
-        editor,
-        {
-          devtools_index:
-            findLastIndex(
-              Node.get(editor, [
-                ...path.slice(0, -1),
-                path[path.length - 1] - 1,
-              ])
-            ) + 1,
-        },
-        { at: path }
-      );
-      console.log({ node: Node.get(editor, path) });
-      console.groupEnd()
-    }
+    //   Node.parent(editor, path).devtools_index !==
+    //     (node.devtools_index as number) - 1
+    // ) {
+    //   console.group("First Child");
+    //   console.log({ node });
+    //   const { devtools_index } = Node.parent(editor, path);
+    //   console.log({ devtools_index });
+    //   Transforms.setNodes(
+    //     editor,
+    //     { devtools_index: (devtools_index as number) + 1 },
+    //     { at: path }
+    //   );
+    //   console.log(Node.get(editor, path));
+    //   console.groupEnd();
+    // }
+
+    // if (
+    //   !DevEditor.isEditor(node) &&
+    //   path[path.length - 1] !== 0 &&
+    //   findLastIndex(
+    //     Node.get(editor, [...path.slice(0, -1), path[path.length - 1] - 1])
+    //   ) +
+    //     1 !==
+    //     node.devtools_index
+    // ) {
+    //   console.group("Other Child");
+    //   console.log({node})
+    //   console.log({
+    //     lastIndex: findLastIndex(
+    //       Node.get(editor, [...path.slice(0, -1), path[path.length - 1] - 1])
+    //     ),
+    //   });
+
+    //   Transforms.setNodes(
+    //     editor,
+    //     {
+    //       devtools_index:
+    //         findLastIndex(
+    //           Node.get(editor, [
+    //             ...path.slice(0, -1),
+    //             path[path.length - 1] - 1,
+    //           ])
+    //         ) + 1,
+    //     },
+    //     { at: path }
+    //   );
+    //   console.log({ node: Node.get(editor, path) });
+    //   console.groupEnd()
+    // }
     normalizeNode(entry);
   };
 
