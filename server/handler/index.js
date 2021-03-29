dfd2da59a9f5d251074880a742ed89be19924db9
Nivekithan S <nivekithan363@gmail.com>
2021-03-29 21:03:40 +0530

    master : start over

--- server/handler/index.js
+++
@@ -1,6 +0,0 @@
-module.exports.postNewGroup = require("./post/postNewGroup");
-module.exports.getGroups = require("./get/getGroups");
-module.exports.putDay = require("./put/putDay");
-module.exports.getTest = require("./get/getTest");
-module.exports.deleteDays = require("./delete/deleteDays");
-module.exports.putTest = require("./put/putTest");
